5b9f82933904415c75ff733578368013729aa240
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-09-20 23:44:16 +0600

    third commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MainHeader from './MainHeader/MainHeader';
+import Content from './Content/Content';
+
+
+
+const Home = () => {
+    return (
+        <div>
+           <MainHeader></MainHeader>
+           <Content></Content>
+        </div>
+    );
+};
+
+export default Home;
+
