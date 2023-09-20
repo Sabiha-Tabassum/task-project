9597d539057024477c1b38394116bd2765c5abc6
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-09-21 00:06:54 +0600

    sixth commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainHeader from './MainHeader/MainHeader';
 import Content from './Content/Content';
+import SideBar from './SideBar/SideBar';
 
 
 
 const Home = () => {
     return (
         <div>
+           <SideBar></SideBar>
            <MainHeader></MainHeader>
            <Content></Content>
         </div>
---
+++ src/Pages/Home/SideBar/SideBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sidebar.css'
+
+const SideBar = () => {
+    return (
+        <div className='sidebar-area'>
+            <div className='sidebar'>
+                <p>Sidebar</p>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
