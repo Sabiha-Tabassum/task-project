6ceebec5672669ab0a7f3996fbbbd680952a75d1
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-09-20 23:47:44 +0600

    fourth commit

--- src/Pages/Home/MainHeader/MainHeader.jsx
+++ src/Pages/Home/MainHeader/MainHeader.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './mainheader.css'
 
 const MainHeader = () => {
     return (
-        <div>
-            
+        <div className='header'>
+            <div className='main-content'>
+                <h1>Main Content</h1>
+            </div>
         </div>
+
     );
 };
 
